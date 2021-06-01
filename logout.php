e78e47b18e0d0ef86c000fed5fa40a45b1abca1b
anwar2731 <85085697+anwar2731@users.noreply.github.com>
2021-06-01 14:32:34 +0700

    Update logout.php
    mengubahh folder save

--- logout.php
+++ logout.php
@@ -2,6 +2,6 @@
 session_start();
 $_SESSION['access_token']="";
 $_SESSION['access_profile']="";
-
+folder save yang akan menjadikan kita lebih kuat dan sejahtera dan sentosa insyaallah
 header('location:index.php');
 ?>
